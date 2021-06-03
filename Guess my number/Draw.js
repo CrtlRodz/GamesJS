d0b9c87b1f87436ee88ad44f91750b78abffd657
CrtlRodz <82580186+CrtlRodz@users.noreply.github.com>
2021-06-03 14:49:49 +0200

    Add files via upload

---
+++ Guess my number/Draw.js
@@ -0,0 +1,43 @@
+'use strict';
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highScore = 0;
+const displayMessage = function(message) {
+    document.querySelector('.message').textContent = message;
+}
+document.querySelector('.check').addEventListener('click', function() {
+    const guess = Number(document.querySelector('.guess').value);
+    console.log(guess);
+    if (!guess) {
+        displayMessage('🚫No Number🚫');
+    } else if (guess === secretNumber) {
+        displayMessage('🙌 Correct Number!🙌');
+        document.querySelector('.number').textContent = secretNumber;
+        document.querySelector('body').style.backgroundColor = '#60b347';
+        document.querySelector('.number').style.width = '30rem';
+        if (score > highScore) {
+            highScore = score;
+            document.querySelector('.highscore').textContent = highScore;
+        }
+    } else if (guess !== secretNumber) {
+        if (score > 1) {
+            displayMessage(guess > secretNumber ? '👆Too HIGH!👆' : '👇Too Low!👇');
+            score--;
+            document.querySelector('.score').textContent = score;
+        } else {
+            displayMessage('😑You lose😑');
+            document.querySelector('.score').textContent = 0;
+        }
+    };
+});
+
+document.querySelector('.again').addEventListener('click', function() {
+    score = 20;
+    secretNumber = Math.trunc(Math.random() * 20) + 1;
+    displayMessage('Start guessing...');
+    document.querySelector('.guess').value = '';
+    document.querySelector('.number').textContent = '?';
+    document.querySelector('body').style.backgroundColor = '#222';
+    document.querySelector('.number').style.width = '15rem';
+    document.querySelector('.score').textContent = score;
+});
